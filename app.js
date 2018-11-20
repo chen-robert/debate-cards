0e8a1ba28d703dbaf0c2aaa116972c013696a15b
Robert Chen <robertchen@live.com>
2018-11-19 22:55:07 -0800

    Uploaded predownloaded data.json

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(enforce.HTTPS({trustProtoHeader: true}));
 }
 
-const data = {};
+const data = require(path.join(__dirname, "src/data.json"));
 load(data, "https://hspolicy.debatecoaches.org");
 load(data, "https://opencaselist.paperlessdebate.com");
 
