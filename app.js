ff3739a357abc6f039822c97364c21e2ba01cfe9
Robert Chen <robertchen@live.com>
2018-11-19 18:35:22 -0800

    Rough sketch of scraper

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const compression = require("compression");
-const enforce = require('express-sslify');
+const enforce = require("express-sslify");
+const {listSchools, listCases, getData} = require(path.join(__dirname, "src/api.js"));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,8 +10,31 @@ const PORT = process.env.PORT || 3000;
 app.use(compression());
 if(process.env.NODE_ENV === "production"){
   app.use(enforce.HTTPS({trustProtoHeader: true}));
-  console.log(process.env.NODE_ENV);
 }
 
-app.use(express.static(path.join(__dirname, "dist")));
+const data = {};
+listSchools()
+  .then(schools => {
+    schools.each((i, {name, href}) => {
+      setTimeout(() => {
+        const schoolData = {};
+        listCases(href)
+          .then(cases => {
+            cases.each((i, {caseName, caseHref}) => {
+              getData(caseHref)
+                .then(data => schoolData[caseName] = data);
+            });
+          });
+        data[name] = schoolData;
+        
+        console.log(`Finished loading ${name}. ${i} out of ${schools.length}`);
+      }, 1000 * i);
+    });
+  });
+  
+
+app.get("/data", (req, res) => res.send(data));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("*", (req, res) => res.redirect("/"));
+
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
---
+++ src/api.js
@@ -0,0 +1,78 @@
+const apiBase = "https://hspolicy.debatecoaches.org";
+const request = require("request-promise").defaults({ jar: true });
+const cheerio = require("cheerio");
+
+
+module.exports = {
+  listSchools: () => {
+    return request(apiBase)
+      .then(html => {
+        const $ = cheerio.load(html);
+        return $(".PanelsSchools .xwikipanelcontents a").map((i, elem) => {
+            return {
+              href: $(elem).attr("href"),
+              name: $(elem).text()
+            }
+        });
+      });
+  },
+  listCases: school => {
+    return request(apiBase + school)
+      .then(html => {
+        const $ = cheerio.load(html);
+        return $("#tblTeams td a").map((i, elem) => {
+            return {
+              caseHref: $(elem).attr("href"),
+              caseName: $(elem).text()
+            }
+        });
+      });
+  },
+  getData: url => {
+    return request(apiBase + url)
+      .then(html => {
+        console.log(`Loaded ${url}`);
+        
+        const $ = cheerio.load(html);
+        const rows = $("#tblRounds tr");
+        const header = rows[0];
+        
+        let reportIndex, docIndex;
+        
+        $(header).children().each((i, elem) => {
+          if($(elem).text() === "Round Report") reportIndex = i;
+          if($(elem).text() === "Open Source") docIndex = i;
+        });
+        
+        return rows.slice(1)
+          .map((i, elem) => {
+            const ret = {
+              report: "",
+              docUrl: undefined
+            };
+            
+            const reportScript = $(
+              $(elem).children()[reportIndex]
+            ).find("img").attr("onclick");
+            
+            if(reportScript !== undefined){
+              if(reportScript.startsWith("showReport('")){
+                const reportNum = reportScript.substring(reportScript.indexOf("'") + 1, reportScript.lastIndexOf("'"));
+                const reportText = $(`#report${reportNum}`).text();
+                
+                ret.report = reportText;
+              }else{
+                console.log(`Invalid url ${url}`);
+              }
+            }
+            
+            ret.docUrl = apiBase + $(
+              $(elem).children()[docIndex]
+            ).find("a").attr("href");
+            
+            return ret;
+          })
+          .get();
+      });
+  }
+}
